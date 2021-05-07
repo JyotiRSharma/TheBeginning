feaa7dbd9dfb4292851b09b51597672309403f13
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2021-05-07 17:29:45 +0530

    Rendering HTML using shortcuts render

--- music/views.py
+++ music/views.py
@@ -1,17 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
 
     album_list = Album.objects.all()
-    template = loader.get_template('music/index.html')
+    context = {'album_list': album_list}
 
-    context = {
-        'album_list': album_list,
-    }
-
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 def details(request, album_id):
     return HttpResponse("This is for the album ID: " + str(album_id))
