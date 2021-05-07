3e7c1e5ac48fac8e1eb384fad004835c8ea55643
JyotiRSharma <appdeveloperubuntu@gmail.com>
2021-05-07 19:58:19 +0530

    details.html now shows logo

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Album
@@ -6,8 +7,12 @@ def index(request):
 
     album_list = Album.objects.all()
     context = {'album_list': album_list}
-
     return render(request, 'music/index.html', context)
 
 def details(request, album_id):
-    return HttpResponse("This is for the album ID: " + str(album_id))
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
+
