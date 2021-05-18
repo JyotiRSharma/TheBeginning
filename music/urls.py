fd66b8a0b3b684b32438afc50b19d3a869698df3
JyotiRSharma <appdeveloperubuntu@gmail.com>
2021-05-18 18:10:56 +0530

    Added a navbar

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,15 @@
 from django.urls import re_path, path
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     re_path(r'^$', views.index, name='index'),
 
-    # /music/71
-    re_path(r'^(?P<album_id>[0-9]+)$', views.details, name='details'),
+    # /music/<album_id>
+    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favourite
+    re_path(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 def index(request):
 
@@ -9,10 +9,27 @@ def index(request):
     context = {'album_list': album_list}
     return render(request, 'music/index.html', context)
 
-def details(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+def detail(request, album_id):
+    # try:
+    #     album = Album.objects.get(id=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist")
+
+    album = get_object_or_404(Album, id=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
+def favourite(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',
+        {
+            'album':album,
+            'error_message': "You didn't select the right song."
+        })
+    else:
+        selected_song.is_favourite=True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
